a95229e3fcc806d43e673d7a234f5d199eba9ae9
Zeph180 <zephrichards1@gmail.com>
2023-06-21 00:31:52 +0300

    Fix linter errors

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,20 +1,20 @@
-import React, { useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRandomGreeting } from '../redux/GreetingSlice';
 
 const Greetings = () => {
   const dispatch = useDispatch();
   const randomGreeting = useSelector((state) => state.greeting.randomGreeting);
-  
-useEffect (() => {
-  dispatch(fetchRandomGreeting());
-}, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchRandomGreeting());
+  }, [dispatch]);
 
   return (
     <div>
       {randomGreeting ? (<p>{randomGreeting}</p>) : (<p>Loading.....</p>)}
     </div>
-    )
-}
+  );
+};
 
-export default Greetings
+export default Greetings;
--- src/redux/GreetingSlice.js
+++ src/redux/GreetingSlice.js
@@ -1,27 +1,28 @@
-import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const apiUrl = "http://localhost:3000/api/random_greeting";
+const apiUrl = 'http://localhost:3000/api/random_greeting';
 
 export const fetchRandomGreeting = createAsyncThunk(
-  "greeting/fetchRandomGreeting",
+  'greeting/fetchRandomGreeting',
   async () => {
     const response = await fetch(apiUrl);
     const data = await response.json();
     return data.greeting;
-  }
+  },
 );
 
 const greetingSlice = createSlice({
-  name: "greeting",
+  name: 'greeting',
   initialState: {
-    randomGreeting: "",
+    randomGreeting: '',
   },
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchRandomGreeting.fulfilled, (state, action) => {
-      state.randomGreeting = action.payload;
+      const newState = state;
+      newState.randomGreeting = action.payload;
     });
-  }
+  },
 });
 
 export default greetingSlice.reducer;
