922f8756cf9ab091a4708d412011b55f2bef488c
Zeph180 <zephrichards1@gmail.com>
2023-06-20 23:48:25 +0300

    Fetch fro api

---
+++ src/redux/GreetingSlice.js
@@ -0,0 +1,27 @@
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+
+const apiUrl = "http://localhost:3000/api/random_greeting";
+
+export const fetchRandomGreeting = createAsyncThunk(
+  "greeting/fetchRandomGreeting",
+  async () => {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.greeting;
+  }
+);
+
+const greetingSlice = createSlice({
+  name: "greeting",
+  initialState: {
+    randomGreeting: "",
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchRandomGreeting.fulfilled, (state, action) => {
+      state.randomGreeting = action.payload;
+    });
+  }
+});
+
+export default greetingSlice.reducer;
